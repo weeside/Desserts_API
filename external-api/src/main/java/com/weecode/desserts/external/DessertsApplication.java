2bc00f728a11b2103e84dcaa36aca45c0062949a
pkch93 <pkch9377@gmail.com>
2020-07-11 13:05:20 +0900

    [DESSERTS-9] external-api 모듈 기본 구성

---
+++ external-api/src/main/java/com/weecode/desserts/external/DessertsApplication.java
@@ -0,0 +1,12 @@
+package com.weecode.desserts.external;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DessertsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DessertsApplication.class, args);
+    }
+}
---
+++ external-api/src/test/java/com/weecode/desserts/external/DessertsApplicationTest.java
@@ -0,0 +1,11 @@
+package com.weecode.desserts.external;
+
+import com.weeside.desserts.test.ContextTest;
+import org.junit.jupiter.api.Test;
+
+class DessertsApplicationTest extends ContextTest {
+
+    @Test
+    void contextLoad() {
+    }
+}
